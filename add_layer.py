63751a0b523d4eb0476142f601266a65630d74d5
lihaha007 <48830572+lihaha007@users.noreply.github.com>
2019-07-15 16:23:50 +0800

    Initial commit

---
+++ add_layer.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+#定义添加神经层的函数def add_layer（），参数有：输入值，输入的大小，输出的大小和激励函数
+def add_layer(imputs,in_size,out_size,activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size,out_size]))
+    biases = tf.Variable(tf.zeros([1,out_size])+0.1)
+    Wx_plus_b = tf.matmul(imputs,Weights)+biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+x_data = np.linspace(-1,1,300)[:,np.newaxis]
+noise = np.random.normal(0,0.05,x_data.shape)
+y_data = np.square(x_data)-0.5 + noise
+
+xs = tf.placeholder(tf.float32,[None,1])
+ys = tf.placeholder(tf.float32,[None,1])
+#构建一个有输入层1个神经元，隐藏层有10个，输出层有一个
+l1 = add_layer(xs,1,10,activation_function=tf.nn.relu)
+prediction = add_layer(l1,10,1,activation_function=None)
+
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction),reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(x_data,y_data)
+plt.ion()   #连续显示
+plt.show()
+
+for i in range(1000):
+    sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
+    if i % 50 == 0:
+        #print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        prediction_value = sess.run(prediction,feed_dict={xs:x_data})
+        lines = ax.plot(x_data,prediction_value,'r-',lw=5)
+        
+        plt.pause(0.1)
